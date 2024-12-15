24dd1b4f419e5af6cd0d846b43a70d7f5ef01ae6
Kevin Vo <foolycooly288622@yahoo.com>
2024-12-15 11:19:31 -0800

    Initial.
    Initial.

---
+++ TequilasRestaurant/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace TequilasRestaurant.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public ICollection<OrderItem> OrderItem{ get; set; }
+    }
+}
---
+++ TequilasRestaurant/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace TequilasRestaurant.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId {  get; set; }
+        public Category? Category { get; set; } // A product belongs to a category
+        public ICollection<OrderItem>? OrderItems { get; set; } // A product can be in many order items
+        public ICollection<ProductIngredient>? ProductIngredients { get; set; } // A product can have many ingredients
+    }
+}
