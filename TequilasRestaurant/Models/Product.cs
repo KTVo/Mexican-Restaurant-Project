1fea5509f894d53d625f0c93d30811ef52a25fd8
Kevin Vo <foolycooly288622@yahoo.com>
2024-12-16 21:53:19 -0800

    about to add profile image to backend
    about to add profile image to backend

--- TequilasRestaurant/Models/Product.cs
+++ TequilasRestaurant/Models/Product.cs
@@ -1,15 +1,24 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TequilasRestaurant.Models
 {
     public class Product
     {
+        static readonly string profile_pic = "https://www.greendrop.com/assets/images/beautiful-tree-with-large-green-top.jpg";
         public int ProductId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public Decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId {  get; set; }
+
+        //This will not map the file to the database with Entity Relationship Document (ERD) migration
+        //Instead, we'll bring over the ImageUrl
+        [NotMapped] 
+        public IFormFile? ImageFile { get; set; }
+        public string ImageUrl { get; set; } = profile_pic;
+
         public Category? Category { get; set; } // A product belongs to a category
         public ICollection<OrderItem>? OrderItems { get; set; } // A product can be in many order items
         public ICollection<ProductIngredient>? ProductIngredients { get; set; } // A product can have many ingredients
